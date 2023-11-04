0fcedcf148b6875f3189d0e1d9e36f2de518cc45
BrayanPisuna <brayanpisuna@gmail.com>
2023-11-04 00:00:03 -0500

    Contorlador de crear usuario

--- usuarios/create.php
+++ usuarios/create.php
@@ -8,32 +8,73 @@ include('../layout/parte1.php');
 
 
 
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-      <!-- Content Header (Page header) -->
-      <div class="content-header">
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
         <div class="container-fluid">
-          <div class="row mb-2">
-            <div class="col-sm-6">
-              <h1 class="m-0">Crear Usuarios</h1>
-            </div><!-- /.col -->
-           
-          </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-      </div>
-      <!-- /.content-header -->
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Registro de un nuevo Usuario</h1>
+                </div><!-- /.col -->
 
-
-      <!-- Main content -->
-      <div class="content">
-        <div class="container-fluid">
-          
-          <!-- /.row -->
+            </div><!-- /.row -->
         </div><!-- /.container-fluid -->
-      </div>
-      <!-- /.content -->
     </div>
-    <!-- /.content-wrapper -->
+    <!-- /.content-header -->
+
+
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+
+
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h3 class="card-title">Llene los datos con cuidado</h3>
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+                        </button>
+                    </div>
+                    <!-- /.card-tools -->
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body" style="display: block;">
+                    <form action="../app/controllers/usuarios/create.php" method="post">
+                        <div class="form-group">
+                            <label>Nombres</label>
+                            <input type="text" class="form-control" id="exampleInputNombre" aria-describedby="Nombre" >
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email Empresarial</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Contraseña</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" >
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Verificar Contraseña</label>
+                            <input type="password" class="form-control" id="exampleInputPassword2">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Cancelar</button>
+                        <button type="submit" class="btn btn-primary">Guardar</button>
+                    </form>
+
+
+                </div>
+                <!-- /.card-body -->
+            </div>
+
+
+
+
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
 
 
 
