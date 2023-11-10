8b292bef66eceaf94e963f74404faea1c79afbeb
BrayanPisuna <brayanpisuna@gmail.com>
2023-11-09 22:19:02 -0500

    Correción

--- app/controllers/usuarios/delete_usuarios.php
+++ app/controllers/usuarios/delete_usuarios.php
@@ -1,17 +1,29 @@
 <?php
 
 include('../../config.php');
-    
-    $id_usuario = $_POST['id_usuario'];
 
-    // Preparar la sentencia con marcadores de posición
-    $sentencia = $conn->prepare("DELETE FROM usuarios WHERE id_usuario = :id_usuario");
-    // Vincular los valores a los marcadores de posición
-    $sentencia->bindParam(':id_usuario', $id_usuario, PDO::PARAM_STR);
-    // Ejecutar la sentencia
-    $sentencia->execute();
+$id_usuario = $_POST['id_usuario'];
+
+// Comprobar que el ID de usuario existe
+$sql = "SELECT * FROM usuarios WHERE id_usuario = :id_usuario";
+$query = $conn->prepare($sql);
+$query->bindParam(':id_usuario', $id_usuario, PDO::PARAM_STR);
+$query->execute();
+
+if ($query->rowCount() == 0) {
     session_start();
-    $_SESSION['mensaje_exito']="Usuario Eliminado";
-    header('Location:'.$URL.'/usuarios/');
+    $_SESSION['mensaje_error'] = 'El ID de usuario no existe';
+    header('Location: ' . $URL . '/usuarios/');
+    exit;
+}
+
+// Eliminar el usuario
+$sentencia = $conn->prepare("DELETE FROM usuarios WHERE id_usuario = :id_usuario");
+$sentencia->bindParam(':id_usuario', $id_usuario, PDO::PARAM_STR);
+$sentencia->execute();
+
+session_start();
+$_SESSION['mensaje_exito'] = 'Usuario eliminado';
+header('Location:' . $URL . '/usuarios/');
 
 ?>
--- app/controllers/usuarios/show_usuario.php
+++ app/controllers/usuarios/show_usuario.php
@@ -1,11 +1,11 @@
 <?php
 
-$id_usuario = $_GET['id'] ?? null;
+$id_usuario_get= $_GET['id'] ?? null;
 
 if (array_key_exists('id', $_GET)) {
     $sql_usuarios = "SELECT us.id_usuario as id_usuario, us.usu_nombres as usu_nombres, us.usu_email as usu_email, rol.rol as rol FROM usuarios as us INNER JOIN roles as rol on us.id_rol = rol.id_rol WHERE us.id_usuario = ?";
     $query_usuarios = $conn->prepare($sql_usuarios);
-    $query_usuarios->bindValue(1, $id_usuario, PDO::PARAM_INT);
+    $query_usuarios->bindValue(1, $id_usuario_get, PDO::PARAM_INT);
     $query_usuarios->execute();
 
     $usuario_datos = $query_usuarios->fetch(PDO::FETCH_ASSOC);
